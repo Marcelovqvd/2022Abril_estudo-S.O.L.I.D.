bb15d12901100bb5e9d81124ade52b4c6e762f04
Marcelo <marcelovqvd@gmail.com>
2022-05-03 17:53:37 -0300

    routes

--- src/routes/students.routes.ts
+++ src/routes/students.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 
+import { Student } from '../domain/entities/student';
+
 const studentsRoutes = Router();
 
-studentsRoutes.get('/', (request, response) => response.json({ msg: 'get' }));
+studentsRoutes.get('/', (request, response) => {
+  const student = new Student();
+
+  console.log(student);
+  response.json({ msg: 'get' });
+});
 
 export { studentsRoutes };
